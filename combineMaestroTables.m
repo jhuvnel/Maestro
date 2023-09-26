0895b5cd2f4c414b451c87170feb8e2fa28bad2e
Ruolan-Sun <58133226+Ruolan-Sun@users.noreply.github.com>
2023-09-25 20:46:21 -0400

    Extract info from Visit3 Pre-activation and Post-activation folder

--- combineMaestroTables.m
+++ combineMaestroTables.m
@@ -10,18 +10,51 @@ MaestroDir = unique(extractfield(dir([MVI_path,filesep,'MVI*_R*',filesep,'Visit*
 all_IFT_tabs = cell(length(MaestroDir),1);
 all_eIFT_tabs = cell(length(MaestroDir),1);
 all_ART_tabs = cell(length(MaestroDir),1);
-for i = 1:length(MaestroDir)        
+extra_IFT_tabs = cell(length(MaestroDir),1);
+extra_eIFT_tabs = cell(length(MaestroDir),1);
+extra_ART_tabs = cell(length(MaestroDir),1);
+extraIFT = 1;
+extraART = 1;
+for i = 1:length(MaestroDir)  
     if isfile([MaestroDir{i},filesep,'IFT-Results.mat'])
         load([MaestroDir{i},filesep,'IFT-Results.mat'],'impedance_data') 
         is_IFT = impedance_data.Current_cu==302.4&impedance_data.Duration_us==26.67;
         all_IFT_tabs{i} = impedance_data(is_IFT,:);
         all_eIFT_tabs{i} = impedance_data(~is_IFT,:);
     end
+    if isfile([MaestroDir{i},filesep,'\Preactivation\IFT-Results.mat'])
+        load([MaestroDir{i},filesep,'\Preactivation\IFT-Results.mat'],'impedance_data') 
+        is_IFT = impedance_data.Current_cu==302.4&impedance_data.Duration_us==26.67;
+        extra_IFT_tabs{extraIFT} = impedance_data(is_IFT,:);
+        extra_eIFT_tabs{extraIFT} = impedance_data(~is_IFT,:);
+        extraIFT = extraIFT + 1;
+        if isfile([MaestroDir{i},filesep,'\Preactivation\ART-Results.mat'])
+            load([MaestroDir{i},filesep,'\Preactivation\ART-Results.mat'],'ART_data')
+            extra_ART_tabs{extraART} = ART_data;
+            extraART = extraART + 1;
+        end
+    end
+    if isfile([MaestroDir{i},filesep,'\Postactivation\IFT-Results.mat'])
+        load([MaestroDir{i},filesep,'\Postactivation\IFT-Results.mat'],'impedance_data') 
+        is_IFT = impedance_data.Current_cu==302.4&impedance_data.Duration_us==26.67;
+        extra_IFT_tabs{extraIFT} = impedance_data(is_IFT,:);
+        extra_eIFT_tabs{extraIFT} = impedance_data(~is_IFT,:);
+        extraIFT = extraIFT + 1;
+        if isfile([MaestroDir{i},filesep,'\Postactivation\ART-Results.mat'])
+            load([MaestroDir{i},filesep,'\Postactivation\ART-Results.mat'],'ART_data')
+            extra_ART_tabs{extraART} = ART_data;
+            extraART = extraART + 1;
+        end
+    end
+
     if isfile([MaestroDir{i},filesep,'ART-Results.mat'])
         load([MaestroDir{i},filesep,'ART-Results.mat'],'ART_data')
         all_ART_tabs{i} = ART_data;
-    end   
+    end       
 end
+all_IFT_tabs = cat(1, all_IFT_tabs, extra_IFT_tabs);
+all_eIFT_tabs = cat(1, all_eIFT_tabs, extra_eIFT_tabs);
+all_ART_tabs = cat(1, all_ART_tabs, extra_ART_tabs);
 temp = sortrows(vertcat(all_IFT_tabs{:}),'Date','Ascend');
 [~,ind] = unique(temp);
 maestro_data.IFT = sortrows(sortrows(temp(ind,:),'Date','Ascend'),'Subject','Ascend');
